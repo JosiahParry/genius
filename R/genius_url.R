2e0b847dd29f5316fe48ca948ad9c094f8d9f623
Josiah <josiah.parry@gmail.com>
2017-05-30 10:32:44 -0400

    make scraping a bit cleaner

--- R/genius_url.R
+++ R/genius_url.R
@@ -1,12 +1,10 @@
-require(rvest)
-
 genius_url <- function(url) {
   gsub(pattern = "<.*?>", 
        replacement = "\n",
        html_node(
          read_html(url),
          ".lyrics")) %>%
+    str_replace_all("\\[.*?\\]|[[:punct:]]", "") %>% 
   read_csv(col_names = "line") %>% 
-  na.omit() %>%
-  mutate(line_num = row_number())
+  na.omit()
 }
