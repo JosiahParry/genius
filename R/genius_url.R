fbdd12e2474019888bc7e479e1ea22077d240a8b
Josiah <josiah.parry@gmail.com>
2017-11-07 15:59:39 -0500

    genius_url change to read_lines, keep punctuation, remove [Intro] [Verse] etc. content

--- R/genius_url.R
+++ R/genius_url.R
@@ -1,13 +1,16 @@
 genius_url <- function(url) {
-  
   session <- html_session(url)
   lyrics <- gsub(pattern = "<.*?>",
                  replacement = "\n",
                  html_node(session, ".lyrics")) %>% 
-    str_replace_all("\\[.*?\\]|[[:punct:]]", "") %>% 
-    read_csv(col_names = "line") %>% 
+    read_lines() %>% 
     na.omit()
+  # Convert to tibble
+  lyrics <- tibble(line = lyrics)
+  # Isolate only lines that contain content
   index <- which(str_detect(lyrics$line, "[[:alnum:]]") == TRUE)
-  return(lyrics[index,])
+  lyrics <- lyrics[index,]
+  # Remove lines with things such as [Intro: person & so and so]
+  return(lyrics[str_detect(lyrics$line, "\\[|\\]") == FALSE, ])
   
 }
