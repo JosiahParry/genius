665dc9f5a5fa4aefce114fbff12b9cd8b09b4f81
Josiah <josiah.parry@gmail.com>
2018-01-27 18:23:08 -0500

    replace - with space for url gen

--- R/genius_lyrics.R
+++ R/genius_lyrics.R
@@ -15,6 +15,7 @@
 genius_lyrics <- function(artist = NULL, song = NULL) {
   song <- str_replace_all(song, "\\s*\\(Ft.[^\\)]+\\)", "") %>%
     str_replace_all("&", "and") %>%
+    str_replace_all("-", " ") %>%
     str_trim()
   song_url <- gen_song_url(artist, song)
   lyrics <- genius_url(song_url) %>%
