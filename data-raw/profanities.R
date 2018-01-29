70d2b51f5d483a79664ed8ec186a06f7c45d4be8
Josiah <josiah.parry@gmail.com>
2018-01-29 16:58:19 -0500

    add data

---
+++ data-raw/profanities.R
@@ -0,0 +1,9 @@
+# Resource from Carnegie Mellon University
+# Taken from Luis Von Ahn's (founder of reCAPTCHA & duoling) resources
+# Sourced from https://www.cs.cmu.edu/~biglou/resources/bad-words.txt
+
+profanities <- tibble(word = read_lines("https://www.cs.cmu.edu/~biglou/resources/bad-words.txt"),
+                      lexicon = "LVA") %>%
+  filter(word != "")
+
+save(profanities, file = "data/profanities.RData")
