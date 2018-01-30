43fa3e34f07db059a87af38ec09505aecfdab41c
Josiah <josiah.parry@gmail.com>
2018-01-30 17:46:57 -0500

    remove lexicon column

--- data-raw/profanities.R
+++ data-raw/profanities.R
@@ -2,8 +2,7 @@
 # Taken from Luis Von Ahn's (founder of reCAPTCHA & duoling) resources
 # Sourced from https://www.cs.cmu.edu/~biglou/resources/bad-words.txt
 
-profanities <- tibble(word = read_lines("https://www.cs.cmu.edu/~biglou/resources/bad-words.txt"),
-                      lexicon = "LVA") %>%
+profanities <- tibble(word = read_lines("https://www.cs.cmu.edu/~biglou/resources/bad-words.txt")) %>%
   filter(word != "")
 
 save(profanities, file = "data/profanities.RData")
