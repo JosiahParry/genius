d3c3ec7538e6344f021bdb566621170511b358bb
Josiah <josiah.parry@gmail.com>
2018-02-09 14:25:46 -0500

    keep track of oddities in urls

--- dev/odd_url_things.R
+++ dev/odd_url_things.R
@@ -2,5 +2,8 @@
 
 "a.k.a" = "aka" # Here a.k.a periods are collapsed
 "M.A.A.D" = "M A A D" # in this scenario periods are stripped and made spaces
-"A$AP" = "A sap"
-"A Curious Tale Of Trials + Persons" = "A Curious Tale Of Trials Persons"
+"A$AP" = "A sap" # $ becomes space
+"A Curious Tale Of Trials + Persons" = "A Curious Tale Of Trials Persons" # + becomes space
+  # Im thinking any of the characters !@#$%^&*()_+ are just ommited
+
+"Snøfall" = "Snfall" # Foreign characters seem to be ommited
