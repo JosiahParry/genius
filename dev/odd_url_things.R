b25e557ca75fca8364540c80a2a8a8ca41a222c1
Josiah <josiah.parry@gmail.com>
2018-02-09 11:31:55 -0500

    keeping track of the oddities in urls

---
+++ dev/odd_url_things.R
@@ -0,0 +1,6 @@
+# Oddities of genius urls
+
+"a.k.a" = "aka" # Here a.k.a periods are collapsed
+"M.A.A.D" = "M A A D" # in this scenario periods are stripped and made spaces
+"A$AP" = "A sap"
+"A Curious Tale Of Trials + Persons" = "A Curious Tale Of Trials Persons"
