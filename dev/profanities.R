52eb844275842caf268f2634296e17fc0fa15042
Josiah <josiah.parry@gmail.com>
2018-02-12 10:38:51 -0500

    update readme

--- dev/profanities.R
+++
@@ -1,8 +0,0 @@
-# Resource from Carnegie Mellon University
-# Taken from Luis Von Ahn's (founder of reCAPTCHA & duoling) resources
-
-profanities <- tibble(word = read_lines("https://www.cs.cmu.edu/~biglou/resources/bad-words.txt"),
-                      lexicon = "LVA") %>%
-  filter(word != "")
-
-write_rds(profanities, "data/profanities.rds")
