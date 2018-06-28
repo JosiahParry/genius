204460036134a4398ac22a72ab6ce18a5ae96fc2
Josiah <josiah.parry@gmail.com>
2018-06-28 13:08:44 -0400

    add roxygen to each util function

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,22 @@
-#' Utility functions
-#'
-#' @import dplyr
-#' @importFrom stringr str_replace_all str_trim
-
+#' Form of genius_album that can handle errors
+#' @importFrom purrr possibly
+#' @importFrom tibble as_tibble
+#' @export
 possible_album <- possibly(genius_album, otherwise = as_tibble())
+
+#' Form of genius_lyrics that can handle errors
+#' @importFrom purrr possibly
+#' @importFrom tibble as_tibble
+#' @export
 possible_lyrics <- possibly(genius_lyrics, otherwise = as_tibble())
 
+
+#' Prepares input strings for `gen_song_url()`
+#'
+#' Applies a number of regular expressions to prepare the input to match Genius url format
+#'
+#' @param input Either artist, song, or album, function input.
+#' @export
 prep_info <- function(input) {
   str_replace_all(input,
                   c("\\s*\\(Ft.[^\\)]+\\)" = "",
