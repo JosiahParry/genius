b3a4ce690a125eda13b578c00b585b9888584c4b
JosiahParry <josiah.parry@gmail.com>
2018-12-23 12:14:42 -0800

    add more cleaning parameters

--- R/utils.R
+++ R/utils.R
@@ -40,7 +40,7 @@ prep_info <- function(input) {
 #' vector used for cleaning lines from urls
 # vector for cleaning names
 cleaning <- function() {
-    clean_vec <- c("([[:alpha:]]{2,})([[:upper:]])" = "\\1\n\\2", # turn camel case into new lines
+    clean_vec <- c("([a-z0-9]{2,})([[:upper:]])" = "\\1\n\\2", # turn camel case into new lines
     "(\\]|\\))([[:upper:]])" = "\\1\n\\2", # letters immediately after closing brackets new lines
     # brackets with producer info into new lines
     "(\\[.{2,100}\\])" ="\n\\1\n",
@@ -48,7 +48,9 @@ cleaning <- function() {
     "’" = "'",
     # if quotes follow or precede brackets fix lines
     "(\\])(\")" = "\\1\n\\2",
-    "(\")(\\[)" = "\\1\n\\2")
+    "(\")(\\[)" = "\\1\n\\2",
+    # if a question mark directly touches a word or number make new lines
+    "(\\?)([[:alpha:]])" = "\\1\n\\2")
 
     return(clean_vec)
 }
