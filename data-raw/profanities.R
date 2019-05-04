5704add11423d5d43fcb4dbaff6c59473bc56759
josiahparry <josiah.parry@gmail.com>
2019-05-04 14:34:14 -0400

    remove data folders

--- data-raw/profanities.R
+++
@@ -1,8 +0,0 @@
-# Resource from Carnegie Mellon University
-# Taken from Luis Von Ahn's (founder of reCAPTCHA & duoling) resources
-# Sourced from https://www.cs.cmu.edu/~biglou/resources/bad-words.txt
-
-profanities <- tibble(word = read_lines("https://www.cs.cmu.edu/~biglou/resources/bad-words.txt")) %>%
-  filter(word != "")
-
-save(profanities, file = "data/profanities.RData")
