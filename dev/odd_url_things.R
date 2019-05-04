515772877a51283e0caee88a2df866330a41a4cd
josiahparry <josiah.parry@gmail.com>
2019-05-04 14:35:38 -0400

    clean up repo

--- dev/odd_url_things.R
+++
@@ -1,9 +0,0 @@
-# Oddities of genius urls
-
-"a.k.a" = "aka" # Here a.k.a periods are collapsed
-"M.A.A.D" = "M A A D" # in this scenario periods are stripped and made spaces
-"A$AP" = "A sap" # $ becomes space
-"A Curious Tale Of Trials + Persons" = "A Curious Tale Of Trials Persons" # + becomes space
-  # Im thinking any of the characters !@#$%^&*()_+ are just ommited
-
-"Snøfall" = "Snfall" # Foreign characters seem to be ommited
